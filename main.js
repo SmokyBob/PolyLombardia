5e178c32d30f8ab6cfdbf06970011ab0b4a49b20
Matteo Danelli <roachteo@gmail.com>
2015-02-07 01:07:35 +0100

    Implemented possibility to catch data from multiple sources, using a dropdown menu. Actually, only museums available

--- main.js
+++ main.js
@@ -4,11 +4,33 @@ var markers = [];
 var tipologia = [];
 var prezzo = [];
 
+var origin = "EMPTY";
+
 var gratis = false;
 
 template.addEventListener('template-bound', function(e) {
 
 	template.url_entry = 'https://www.dati.lombardia.it/resource/iu2c-9p5j.json';
+	template.source_name = ['Musei','TODO1','TODO2'];
+	template.source_url = ['https://www.dati.lombardia.it/resource/iu2c-9p5j.json','TODO','TODO'];
+	template.origin = "";
+
+	template.selectOrigin = function(e) {
+		switch (e.detail.item.id) {
+			case 'Musei': 
+				origin = template.source_url[0];
+				break;
+			case 'TODO1': 
+				origin = template.source_url[1];
+				break;
+			case 'TODO2': 
+				origin = template.source_url[2];
+				break;
+		};
+
+		console.log(origin);
+		template.origin = origin;
+	}
 
 	template.select = function() {
 		markers = [];
@@ -75,11 +97,6 @@ template.addEventListener('template-bound', function(e) {
 		template.province = province;
 		template.markers = markers;
 		template.tipologia = tipologia;
-
-
-
 	}
 
 });
-
-
